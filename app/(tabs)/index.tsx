c591cac6988125a1838eb1a4f18d2e6a1152874c
tekibo <tekibohelp@gmail.com>
2025-06-13 16:07:12 +0530

    :sparkles: nativewind working

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="font-bold text-2xl">Hi mom!</Text>
     </View>
   )
 }
